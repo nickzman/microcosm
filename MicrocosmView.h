fbf289b21fbdd6f8c5e433aab80c577b3215d6a3
Nick Zitzmann <nickzman@66.187.131.25.in-addr.arpa>
2010-06-30 00:46:40 -0600

    Forgot I can't use ObjC properties without blowing away backward compatibility

--- MicrocosmView.h
+++ MicrocosmView.h
@@ -6,6 +6,23 @@
 //  Copyright (c) 2010, __MyCompanyName__. All rights reserved.
 //
 
+/* This file is part of Microcosm.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #import <ScreenSaver/ScreenSaver.h>
 #import "Microcosm/Microcosm.h"
 #import "util/rsTimer.h"
@@ -27,17 +44,29 @@
 - (IBAction)closeSheet:(id)sender;
 - (IBAction)reset:(id)sender;
 
-@property(assign,nonatomic) int singleTime;
-@property(assign,nonatomic) int kaleidoscopeTime;
-@property(assign,nonatomic) int background;
-@property(assign,nonatomic) int resolution;
-@property(assign,nonatomic) int depth;
-@property(assign,nonatomic) int fov;
-@property(assign,nonatomic) int gizmoSpeed;
-@property(assign,nonatomic) int colorSpeed;
-@property(assign,nonatomic) int cameraSpeed;
-@property(assign,nonatomic) BOOL shaders;
-@property(assign,nonatomic) BOOL fog;
-@property(assign,nonatomic) BOOL mainScreenOnly;
+- (int)singleTime;
+- (void)setSingleTime:(int)st;
+- (int)kaleidoscopeTime;
+- (void)setKaleidoscopeTime:(int)kTime;
+- (int)background;
+- (void)setBackground:(int)bg;
+- (int)resolution;
+- (void)setResolution:(int)r;
+- (int)depth;
+- (void)setDepth:(int)d;
+- (int)fov;
+- (void)setFov:(int)f;
+- (int)gizmoSpeed;
+- (void)setGizmoSpeed:(int)gs;
+- (int)colorSpeed;
+- (void)setColorSpeed:(int)cs;
+- (int)cameraSpeed;
+- (void)setCameraSpeed:(int)cs;
+- (BOOL)shaders;
+- (void)setShaders:(BOOL)shaders;
+- (BOOL)fog;
+- (void)setFog:(BOOL)fog;
+- (BOOL)mainScreenOnly;
+- (void)setMainScreenOnly:(BOOL)mso;
 
 @end
--- MicrocosmView.mm
+++ MicrocosmView.mm
@@ -6,9 +6,25 @@
 //  Copyright (c) 2010, __MyCompanyName__. All rights reserved.
 //
 
+/* This file is part of Microcosm.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #import "MicrocosmView.h"
 #import "Microcosm/extensions.h"
-#import <AGL/agl.h>
 
 inline BOOL canUseShaders(void)
 {
@@ -25,8 +41,6 @@ inline BOOL canUseShaders(void)
 @end
 
 @implementation MicrocosmView
-
-@synthesize mainScreenOnly = lMainScreenOnly;
 
 - (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
@@ -407,6 +421,18 @@ inline BOOL canUseShaders(void)
 - (void)setFog:(BOOL)fog
 {
 	lSettings.dFog = fog;
+}
+
+
+- (BOOL)mainScreenOnly
+{
+	return lMainScreenOnly;
+}
+
+
+- (void)setMainScreenOnly:(BOOL)mso
+{
+	lMainScreenOnly = mso;
 }
 
 @end
